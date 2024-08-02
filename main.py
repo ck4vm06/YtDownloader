5995c4a4383a7de0b0cbdf428522f45f5ff61545
ck4vm06 <chen20061007@gmail.com>
2024-08-03 00:02:37 +0800

    v0.0.0

---
+++ main.py
@@ -0,0 +1,168 @@
+import os
+import threading
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import ttk
+import ytdl
+from pytubefix import Playlist, YouTube
+
+bg = '#1a1a1a'
+fg = 'white'
+itbg = '#404040'
+Yt_URL = 'unknown'
+save_path = 'unknown'
+file_ext = 'unknown' # mp3 mp4
+download_type = 'unknown' # 'list' or 'solo'
+
+def progress_bar_update(progress: int):
+    progress_bar['value'] = progress
+def information_update(info: str):
+    information.config(text=info)
+    app.update_idletasks()
+
+def url_check():
+    url = url_en.get()
+    global download_type
+    global Yt_URL
+    if 'playlist' in url:
+        try:
+            playlist = Playlist(url)
+            information_update(f'[{playlist.title}] is a playlist')
+            Yt_URL = url
+            download_type = 'list'
+            return True
+        except Exception as e:
+            information_update(f'URL error:{e}')
+            return False
+    else:
+        try:
+            yt = YouTube(url)
+            information_update(f'[{yt.title}] is a video')
+            Yt_URL = url
+            download_type = 'solo'
+            return True
+        except Exception as e:
+            information_update(f'URL error:{e}')
+            return False
+def path_check():
+    path = path_en.get()
+    if not os.path.exists(path):
+        information_update("Folder not found")
+        return False
+    else:
+        global save_path
+        save_path = path
+        return True
+def ext_check():
+    ext = combo_box.get().split()
+    global file_ext
+    if ext[1] == 'mp3':
+        file_ext = combo_box.get()
+        return True
+    elif ext[1] == 'mp4':
+        file_ext = combo_box.get()
+        return True
+    else:
+        information_update('Choose mp3 or mp4')
+        return False
+
+def set_url():
+    url_en.delete(0, tk.END)
+    url_en.insert(0, app.clipboard_get())
+
+def set_path():
+    path_en.delete(0, tk.END)
+    path_en.insert(0, filedialog.askdirectory())
+
+def ui_state(is_running: bool):
+    states = {
+        'idle_ui': 'normal' if is_running else 'disabled',
+        'runing_ui': 'normal' if not is_running else 'disabled'
+    }
+    url_en.config(state=states['idle_ui'])
+    url_btn.config(state=states['idle_ui'])
+    path_en.config(state=states['idle_ui'])
+    path_btn.config(state=states['idle_ui'])
+    stop_btn.config(state=states['runing_ui'])
+    combo_box.config(state=states['idle_ui'])
+    down_btn.config(state=states['idle_ui'])
+    url_check_btn.config(state=states['idle_ui'])
+
+def download():
+    url_state = url_check()
+    pathl_state = path_check()
+    extl_state = ext_check()
+    if url_state & pathl_state & extl_state:
+        ui_state(False)
+        global downloader
+        downloader = ytdl.YtDownloader(Yt_URL, save_path, file_ext, information_update, progress_bar_update, download_finish)
+        download_thread = threading.Thread(target=downloader.start)
+        download_thread.start()
+def download_finish():
+    ui_state(True)
+    app.update_idletasks()
+
+def stop_download():
+    global downloader
+    if downloader:
+        downloader.stop_set()
+        del downloader
+
+
+if __name__ == "__main__":
+    app = tk.Tk()
+    app.geometry('480x300')
+    app.resizable(width=False, height=True)
+    app.config(background=bg)
+    app.attributes('-alpha', 0.87)
+    app.title('YtDownload')
+    # 網址
+    url_en_lb = tk.Label(app, text='Youtube URL', bg=bg, fg='white')
+    url_en_lb.grid(column=0, row=0, padx=10, pady=5)
+
+    url_en = tk.Entry(bg=itbg, fg='white' ,font='微軟正黑體 10')
+    url_en.grid(column=1, row=0, padx=10, pady=5, columnspan=2, sticky="ew")
+
+    url_btn = tk.Button(app, bg=itbg, fg='white', text='paste' ,font='微軟正黑體 10', command=set_url)
+    url_btn.grid(column=3, row=0, padx=10, pady=5)
+
+    # 路徑
+    path_en_lb = tk.Label(app, text='Save Path', bg=bg, fg='white')
+    path_en_lb.grid(column=0, row=1, padx=10, pady=5)
+
+    path_en = tk.Entry(bg=itbg, fg='white' ,font='微軟正黑體 10')
+    path_en.grid(column=1, row=1, padx=10, pady=5, columnspan=2, sticky="ew")
+
+    path_btn = tk.Button(app, bg=itbg, fg='white', text='...' ,font=10 , command=set_path)
+    path_btn.grid(column=3, row=1, padx=10, pady=5)
+
+
+    # 選項
+    stop_btn = tk.Button(app, bg=itbg, fg='white', text='X', font='微軟正黑體 10', command=stop_download, state='disabled')
+    stop_btn.grid(column=0, row=2, padx=10, pady=5)
+
+    options = ["Default mp3", "4320p mp4", '2160p mp4', '1440p mp4', '1080p mp4', '720p mp4', '480p mp4', '360p mp4',
+               '240p mp4', '144p mp4']
+    combo_box = ttk.Combobox(app, font='微軟正黑體 10', values=options)
+    combo_box.grid(column=1, row=2, padx=10, pady=5)
+
+    down_btn = tk.Button(app, bg=itbg, fg='white', text='Download' ,font='微軟正黑體 10', command=download)
+    down_btn.grid(column=2, row=2, padx=10, pady=5)
+
+    url_check_btn = tk.Button(app, bg=itbg, fg='white', text='check', font='微軟正黑體 10', command=url_check)
+    url_check_btn.grid(column=3, row=2, padx=10, pady=5)
+
+    #資訊欄
+    group = tk.LabelFrame(app, text='information', bg=bg, fg='white', padx=10, pady=5)
+    group.grid(column=0, row=3, padx=10, pady=5, columnspan=4, sticky="nsew")
+
+    progress_bar = ttk.Progressbar(group, mode='determinate')
+    progress_bar.pack(side='top', fill='x', expand=True, padx=10, pady=10)
+
+    information = (tk.Label(group, text='', bg=bg, fg='white', wraplength=380))
+    information.pack(side='top', fill='both', expand=True, padx=10, pady=10)
+
+
+    # process = (tk.Label(group, text='0/0', bg=bg, fg='white'))
+    # process.pack(side='right')
+    app.mainloop()
---
+++ ytdl.py
@@ -0,0 +1,210 @@
+from moviepy.editor import VideoFileClip, AudioFileClip
+from pytubefix import Playlist, YouTube
+from proglog import ProgressBarLogger
+import multiprocessing
+import os
+import threading
+
+class YtDownloader():
+    def __init__(self, Yt_url, save_path, file_ext, information_update,progress_bar_update , download_finish):
+        self.Yt_url = Yt_url
+        self.save_path = save_path
+        self.file_ext = file_ext.split()# (quality type pack_speed) be like (1080p mp4 fast)
+        self.information_update = information_update
+        self.progress_bar_update = progress_bar_update
+        self.download_finish = download_finish
+
+        self.stop_event = threading.Event()
+
+        self.info_pack = {'counter': '', #(6/9)
+                          'title': '', #song name
+                          'state': '', #Downloading/Converting
+                          'type': ''} #audio/video/mp3/mp4
+
+    def start(self):
+        if 'playlist' in self.Yt_url:
+            video_urls = Playlist(self.Yt_url).video_urls
+            self.__download_manager(video_urls)
+        else:
+            video_url = [self.Yt_url]
+            self.__download_manager(video_url)
+    def stop_set(self):
+        self.stop_event.set()
+
+    def stop(self):
+        self.information_update("Download stopped!")
+        self.download_finish()
+
+    def __download_manager(self, video_urls):
+        print(video_urls)
+        playlist_quantity = len(video_urls)
+        for count, video_url in enumerate(video_urls, start=1):
+            if self.stop_event.is_set():
+                self.stop()
+                return
+            yt = YouTube(video_url, on_progress_callback=self.register_on_progress_callback)
+            self.info_pack['title'] = yt.title
+            self.info_pack['counter'] = f'({count}/{playlist_quantity})'
+            if 'mp3' in self.file_ext:
+                print(self.get_mp3(yt))
+            elif 'mp4' in self.file_ext:
+                print(self.get_mp4(yt))
+        if not self.stop_event.is_set():
+            self.information_update('Download Completed!')
+        self.download_finish()
+
+
+    def register_on_progress_callback(self, stream, chunk, remains):
+        self.progress_bar_update((stream.filesize - remains) / stream.filesize * 100)
+        if self.stop_event.is_set():
+            raise StopException("Download stopped by user.")
+    def information_updater(self):
+        i = self.info_pack
+        self.information_update(f'{i['counter']}{i['title']}\n{i['state']}>>>{i['type']}')
+
+
+    def get_mp3(self, yt):
+        try:
+            abr = self.file_ext[0]
+            audio_file = self.__get_audio(yt, abr)
+            if audio_file.startswith('Error') or audio_file == None:
+                return audio_file
+
+            # to mp3
+            self.info_pack['state'] = 'Reading'
+            self.info_pack['type'] = 'mp3'
+            self.information_updater()#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+            out_file_name = os.path.join(self.save_path, f'{yt.title}.mp3')
+            audio_clip = AudioFileClip(audio_file)
+            audio_clip.write_audiofile(out_file_name,
+                                       bitrate='160k',
+                                       logger=MoviepyBarLogger(self))
+            os.remove(audio_file)
+            return 'Finish'
+        except StopException:
+            print('stop test')
+            self.stop()
+        except Exception as e:
+            return f'Error >>> {e}'
+        try:
+            os.remove(audio_file)
+        except Exception as e:
+            print(e)
+
+    def __get_audio(self, yt, abr):
+
+        self.info_pack['state'] = 'Downloading'
+        self.info_pack['type'] = 'audio resource'
+        self.information_updater()#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+        try:
+            #find stream
+            stream = yt.streams.filter(adaptive=True, type='audio',
+                                           abr=abr).first()
+            if not stream:
+                stream = yt.streams.filter(adaptive=True, type='audio',
+                                           ).order_by('abr').desc().first()
+            print(stream)
+            audio_file = stream.download(output_path=self.save_path,
+                                       filename=f'{stream.title}(ProcessingAudio).{stream.subtype}')
+            return audio_file
+        except StopException:
+            print('stop test')
+            self.stop()
+        except Exception as e:
+            print(e)
+            return f'Error >>> {e}'
+
+
+    def get_mp4(self, yt):
+        try:
+            quality = self.file_ext[0].split('p')
+            res = None if (quality[0] == '') else quality[0]+'p'
+            fps = None if (quality[1] == '') else quality[1]
+
+            abr = '160kbps'
+            audio_file = self.__get_audio(yt, abr)
+            if audio_file.startswith('Error') or audio_file == None:
+                return audio_file
+
+            video_file = self.__get_video(yt, res, fps)
+            if video_file.startswith('Error') or audio_file == None:
+                return video_file
+            # pack_speed = self.file_ext[2] if len(self.file_ext) > 2 else None # 確認使否指定壓縮速率
+
+            self.info_pack['state'] = 'Reading'
+            self.info_pack['type'] = 'mp4'
+            self.information_updater()#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+            out_file_name = os.path.join(self.save_path, f'{yt.title}.mp4')
+            audio = AudioFileClip(audio_file)
+            video = VideoFileClip(video_file)
+            final_video = video.set_audio(audio)
+            final_video.write_videofile(out_file_name,
+                                        codec="libx264",
+                                        audio_codec="aac",
+                                        audio_bitrate='160k',
+                                        preset='fast',
+                                        logger=MoviepyBarLogger(self),
+                                        threads=multiprocessing.cpu_count()
+                                        )
+            os.remove(audio_file)
+            os.remove(video_file)
+            return 'Finish'
+        except StopException:
+            print('stop test')
+            self.stop()
+        except Exception as e:
+            return f'Error >>> {e}'
+
+        try:
+            os.remove(audio_file)
+            os.remove(video_file)
+        except Exception as e:
+            print(e)
+
+
+    def __get_video(self, yt, res, fps):
+        res_list = ['4320p', '2160p', '1440p', '1080p', '720p', '480p', '360p', '240p', '144p']
+
+        self.info_pack['state'] = 'Downloading'
+        self.info_pack['type'] = 'video resource'
+        self.information_updater()#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        print(res)
+        if res not in res_list:
+            res = res_list[2]
+        try:
+            for res_try in res_list[res_list.index(res):]:
+                stream = yt.streams.filter(adaptive=True, type='video',
+                                           res=res_try, fps=fps).desc().first()
+                if not stream:
+                    stream = yt.streams.filter(adaptive=True, type='video',
+                                               res=res_try).desc().first()
+                if stream:
+                    print(stream)
+                    video_file = stream.download(output_path=self.save_path,
+                                               filename=f'{stream.title}(ProcessingVideo).{stream.subtype}')
+                    break
+            return video_file
+        except StopException:
+            print('stop test')
+            self.stop()
+        except Exception as e:
+            print(e)
+            return f'Error >>> {e}' #回傳下載時的錯誤
+
+class MoviepyBarLogger(ProgressBarLogger):
+    def __init__(self, outer_instance):
+        super().__init__()
+        self.outer_instance = outer_instance
+    def bars_callback(self, bar, attr, value, old_value=None):
+        self.outer_instance.progress_bar_update(int((value / self.bars[bar]['total']) * 100))
+        if bar == 't' and value == 0:
+            self.outer_instance.info_pack['state'] = 'Converting'
+            self.outer_instance.information_updater()
+        if self.outer_instance.stop_event.is_set():
+            raise StopException("Download stopped by user.")
+
+class StopException(Exception):
+    pass
